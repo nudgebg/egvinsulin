52e3b9e9ae7cc2d7c5beed43f16edef8c358658b
Rachel  Brandt <rachelbrandt@Rachels-MacBook-Pro.local>
2024-04-18 16:33:18 -0500

    added conext in comments

--- save_data_as.py
+++ save_data_as.py
@@ -1,4 +1,6 @@
 def save_data_as(data,file_format,export_filename):
+    #input data should have an insulin delivery (basal+boluses) titled 'insulin', a CGM column titled 'egv',
+    #a datetime column titled 'datetime', and a patient id column titled 'PtID'
     #data is a dataframe of cleaned data that needs to be saved
     #fileformat is the format of the saved data: 
     #   For a .mat file specify 'MAT'
