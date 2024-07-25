c4e441a13618cdc65f76ad1f590a506ba46f9775
Rachel Brandt <brandtre2@gmail.com>
2024-07-25 18:27:51 -0500

    [30-modularize-iobp2-code] removes basal history and combines basal delivery with boluses

--- studies/iobp2.py
+++ studies/iobp2.py
@@ -8,6 +8,8 @@ class IOBP2StudyData(StudyDataset):
 
     def __init__(self, study_name: str, study_path: str):
         super().__init__(study_path, study_name)
+        #in place for testing purposes
+        self.iletFilePath = self.study_path
         self.iletFilePath = os.path.join(self.study_path, 'Data Tables', 'IOBP2DeviceiLet.txt')
         if not os.path.exists(self.iletFilePath):
             raise FileNotFoundError(f"File not found: {self.iletFilePath}")
@@ -15,12 +17,12 @@ class IOBP2StudyData(StudyDataset):
     def load_data(self) -> pd.DataFrame:
         
         self.df = pd.read_csv(self.iletFilePath, sep="|", low_memory=False,
-               usecols=['PtID', 'DeviceDtTm', 'CGMVal', 'BGTarget', 'BasalDelivPrev',
+               usecols=['PtID', 'DeviceDtTm', 'CGMVal', 'BasalDelivPrev',
                 'BolusDelivPrev','MealBolusDelivPrev'],
                dtype={'PtID': str, 'CGMVal': float})
         
         self.df.rename(columns={'PtID': 'patient_id', 'DeviceDtTm': 'datetime', 'CGMVal': 'cgm', 
-                        'BGTarget': 'bg_target', 'BasalDelivPrev': 'basal_rate', 
+                        'BasalDelivPrev': 'basal_rate', 
                         'BolusDelivPrev': 'bolus', 'MealBolusDelivPrev': 'meal_bolus'}, inplace=True)
         
         #date time strings wiithout time component are assumed to be midnight
@@ -32,7 +34,7 @@ class IOBP2StudyData(StudyDataset):
         self.df['delivery_duration'] = pd.to_timedelta('5 minutes')
         
         #Bolus delivery is separated into two different columns: bolus and meal bolus. These two columns are combined to give a single bolus column
-        self.df['bolus'] = self.df['bolus'] + self.df['meal_bolus']
+        self.df['bolus'] = self.df['bolus'] + self.df['meal_bolus'] + self.df['basal_rate'] #basal delivery in iobp2 is delivered like a bolus, therefore it is added to the bolus column
 
     def _extract_bolus_event_history(self):
         bolus_history = self.df[['patient_id', 'datetime', 'bolus', 'delivery_duration']]
@@ -43,17 +45,17 @@ class IOBP2StudyData(StudyDataset):
 
         return bolus_history.dropna()
 
-    def _extract_basal_event_history(self):
-        basal_history = self.df[['patient_id', 'datetime', 'basal_rate']]
+    # def _extract_basal_event_history(self):
+    #     basal_history = self.df[['patient_id', 'datetime', 'basal_rate']]
         
-        #insulin delivery is reported as the previous amount delivered. Therefore data is shifted to to align with algorithm announcement
-        basal_history['datetime'] = basal_history['datetime'] - timedelta(minutes=5)
-        basal_history['datetime'] = basal_history['datetime'].astype("datetime64[ns]")
-        #convert 5 minute basal deliveries to a hourly basal rate
-        basal_history['basal_rate'] = basal_history['basal_rate'] * 12
-        basal_history['basal_rate'] = basal_history['basal_rate'].fillna(0)
+    #     #insulin delivery is reported as the previous amount delivered. Therefore data is shifted to to align with algorithm announcement
+    #     basal_history['datetime'] = basal_history['datetime'] - timedelta(minutes=5)
+    #     basal_history['datetime'] = basal_history['datetime'].astype("datetime64[ns]")
+    #     #convert 5 minute basal deliveries to a hourly basal rate
+    #     basal_history['basal_rate'] = basal_history['basal_rate'] * 12
+    #     basal_history['basal_rate'] = basal_history['basal_rate'].fillna(0)
         
-        return basal_history.dropna()
+    #     return basal_history.dropna()
 
     def _extract_cgm_history(self):
         cgm_history = self.df[['patient_id', 'datetime', 'cgm']]
@@ -68,12 +70,12 @@ class IOBP2StudyData(StudyDataset):
 if __name__ == '__main__':
     current_dir = os.path.dirname(__file__)
     path = os.path.join(current_dir, 'IOBP2 RCT Public Dataset')
-    
+
     study = IOBP2StudyData(study_name='IOBP2', study_path=path)
     study.load_data()
     bolus_history = study.extract_bolus_event_history()
-    basal_history = study.extract_basal_event_history()
+    # basal_history = study.extract_basal_event_history()
     cgm_history = study.extract_cgm_history()
     print(cgm_history.head())
-    print(basal_history.head())
+    # print(basal_history.head())
     print(bolus_history.head())
