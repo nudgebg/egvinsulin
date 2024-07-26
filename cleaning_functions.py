a87d39d2c74147ffe7e70ae0f9d3a6bb5527a436
Rachel Brandt <brandtre2@gmail.com>
2024-07-25 19:12:54 -0500

    [30-modularize-iobp2-code] bug fix for patient_id being filled with 0s

--- cleaning_functions.py
+++ cleaning_functions.py
@@ -61,7 +61,8 @@ def bolus_transform(bolus_data):
                         
     #fill nans with 0
     bolus_data.patient_id = bolus_data.patient_id.ffill()
-    bolus_data = bolus_data.fillna(0)
+    bolus_data = bolus_data.dropna(subset=['patient_id'])
+    bolus_data.bolus = bolus_data.bolus.fillna(0)
 
     return bolus_data
 
