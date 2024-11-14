2413d97974dad560c32441314ddd68e23d904a10
Jan Wrede <wrede.jan@gmail.com>
2024-11-14 16:09:04 +0100

    [56-accellerate-resampling] profile execution times

--- run_functions.py
+++ run_functions.py
@@ -89,105 +89,114 @@ def current_time():
   return datetime.now().strftime("%H:%M:%S")
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(message)s',
-    datefmt='[%H:%M:%S]',  # Remove milliseconds
-    handlers=[
-        #logging.FileHandler("app.log"),  # Log to a file
-        logging.StreamHandler()  # Also log to console
-    ]
-)
+def main(test=False):
+  logging.basicConfig(
+      level=logging.INFO,
+      format='%(asctime)s %(message)s',
+      datefmt='[%H:%M:%S]',  # Remove milliseconds
+      handlers=[
+          #logging.FileHandler("app.log"),  # Log to a file
+          logging.StreamHandler()  # Also log to console
+      ]
+  )
 
-#run_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-current_dir = os.getcwd()
-in_path = os.path.join(current_dir, 'data/raw')
-out_path = os.path.join(current_dir, 'data/out')
-logging.info(f"Looking for study folders in {in_path} and saving results to {out_path}")
+  #run_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+  current_dir = os.getcwd()
+  if test:
+      in_path = os.path.join(current_dir, 'data/test')
+  else:
+     in_path = os.path.join(current_dir, 'data/raw')
+  out_path = os.path.join(current_dir, 'data/out')
+  logging.info(f"Looking for study folders in {in_path} and saving results to {out_path}")
 
-#define how folders are identified and processed
-patterns = {'IOBP2 RCT Public Dataset': IOBP2StudyData,
-            'FLAIRPublicDataSet': Flair,
-            'PEDAP Public Dataset - Release 3 - 2024-09-25': PEDAP,
-            'DCLP3 Public Dataset - Release 3 - 2022-08-04': DCLP3}
+  #define how folders are identified and processed
+  patterns = {'IOBP2 RCT Public Dataset': IOBP2StudyData,
+              'FLAIRPublicDataSet': Flair,
+              'PEDAP Public Dataset - Release 3 - 2024-09-25': PEDAP,
+              'DCLP3 Public Dataset - Release 3 - 2022-08-04': DCLP3}
 
-# Filter and log folders that cannot be matched
-study_folders = [f for f in os.listdir(in_path) if os.path.isdir(os.path.join(in_path, f))]
-unmatched_folders = []
-matched_folders = []
+  # Filter and log folders that cannot be matched
+  study_folder_names = [f for f in os.listdir(in_path) if os.path.isdir(os.path.join(in_path, f))]
+  unmatched_folders = []
+  matched_folders = []
 
-for folder in study_folders:
-    study_class = None
-    for pattern, handler in patterns.items():
-        if pattern in folder:
-            study_class = handler
-            matched_folders.append((folder, study_class))
-            break
-    if study_class is None:
-        unmatched_folders.append(folder)
+  for folder in study_folder_names:
+      study_class = None
+      for pattern, handler in patterns.items():
+          if pattern in folder:
+              study_class = handler
+              matched_folders.append((folder, study_class))
+              break
+      if study_class is None:
+          unmatched_folders.append(folder)
 
-if unmatched_folders:
-    logging.warning(f"The folders '{unmatched_folders}' are not recognized as a supported studies. \n Did you accidentally rename them? \n Please check the documentation for supported studies.")
-if not matched_folders:
-    logging.error("No supported studies found in the data/raw folder. Exiting.")
-    exit()
+  if unmatched_folders:
+      logging.warning(f"The folders '{unmatched_folders}' are not recognized as a supported studies. \n Did you accidentally rename them? \n Please check the documentation for supported studies.")
+  if not matched_folders:
+      logging.error("No supported studies found in the data/raw folder. Exiting.")
+      exit()
 
-# Process matched folders with progress indicators
-logging.info(f"Start processing supported study folders: {[folder for folder, _ in matched_folders]}")
-with tqdm(total=len(matched_folders)*6, desc=f"{study_class.__name__}", bar_format='Step {n_fmt}/{total_fmt} [{desc}]:|{bar}', unit="step", leave=False) as progress:
-  for folder, study_class in matched_folders:
+  # Process matched folders with progress indicators
+  logging.info(f"Start processing supported study folders:")
+  for i,(folder, study_class) in enumerate(matched_folders):
+      logging.info(f'{i}: {folder} using {study_class.__name__}')
+  logging.info("")
+
+  with tqdm(total=len(matched_folders)*7, desc=f"Processing studies", bar_format='Step {n_fmt}/{total_fmt} [{desc}]:|{bar}', unit="step", leave=False) as progress:
+    for folder, study_class in matched_folders:
       tqdm.write(f"[{current_time()}] Processing {folder} ...")
 
-      out_path_study = os.path.join(out_path, folder)
-      if not os.path.exists(out_path_study):
-          os.makedirs(out_path_study)
-          logging.info(f"Created folder {out_path_study}")
+      study_output_path = os.path.join(out_path, folder)
+      if not os.path.exists(study_output_path):
+          os.makedirs(study_output_path)
       
-      #load data
-      progress.set_description_str(f"{study_class.__name__}: (Loading data)")
-      absolute_folder_path = os.path.join(in_path, folder)
-      study = study_class(study_path=absolute_folder_path)
+      study = study_class(study_path=os.path.join(in_path, folder))
+      process_folder(study, out_path, progress)
+    tqdm.write("Processing complete.")
+
+def process_folder(study,  out_path_study, progress):
+    
+      progress.set_description_str(f"{study.__class__.__name__}: (Loading data)")
       study.load_data()
       tqdm.write(f"[{current_time()}] [x] Data loaded")
       progress.update(1)
 
-      #extract, resample and save
-      progress.set_description_str(f"{study_class.__name__}: Extracting boluses")
+      progress.set_description_str(f"{study.__class__.__name__}: Extracting boluses")
       bolus_history = study.extract_bolus_event_history()
       out_file_path = save_data_as(bolus_history, 'CSV', os.path.join(out_path_study, f"bolus_history"))
       tqdm.write(f"[{current_time()}] [x] Boluses extracted: {out_file_path.split('/')[-1]}")
       progress.update(1)
 
-      progress.set_description_str(f"{study_class.__name__}: Resampling boluses")
+      progress.set_description_str(f"{study.__class__.__name__}: Resampling boluses")
       bolus_history_transformed = bolus_history.groupby('patient_id').apply(pp.bolus_transform, include_groups=False).reset_index(level=0)
       out_file = save_data_as(bolus_history_transformed, 'CSV', os.path.join(out_path_study, f"bolus_history-transformed"))
       tqdm.write(f"[{current_time()}] [x] Boluses resampled: {out_file.split('/')[-1]}")
       progress.update(1)
 
-      progress.set_description_str(f"{study_class.__name__}: Extracting basals")
+      progress.set_description_str(f"{study.__class__.__name__}: Extracting basals")
       basal_history = study.extract_basal_event_history()
       out_file_path = save_data_as(basal_history, 'CSV', os.path.join(out_path_study, f"basal_history"))
       tqdm.write(f"[{current_time()}] [x] Basal events extracted: {out_file_path.split('/')[-1]}")
       progress.update(1)
 
-      progress.set_description_str(f"{study_class.__name__}: Resampling basals")
+      progress.set_description_str(f"{study.__class__.__name__}: Resampling basals")
       basal_history_transformed = basal_history.groupby('patient_id').apply(pp.basal_transform, include_groups=False).reset_index(level=0)
       out_file = save_data_as(basal_history_transformed, 'CSV', os.path.join(out_path_study, f"basal_history-transformed"))
       tqdm.write(f"[{current_time()}] [x] Basal events resampled: {out_file.split('/')[-1]}")
       progress.update(1)
 
-      progress.set_description_str(f"{study_class.__name__}: Extracting glucose")
+      progress.set_description_str(f"{study.__class__.__name__}: Extracting glucose")
       cgm_history = study.extract_cgm_history()
       out_file_path = save_data_as(cgm_history, 'CSV', os.path.join(out_path_study, f"cgm_history"))
       tqdm.write(f"[{current_time()}] [x] CGM extracted: {out_file_path.split('/')[-1]}")
       progress.update(1)
 
-      progress.set_description_str(f"{study_class.__name__}: Resampling glucose")
+      progress.set_description_str(f"{study.__class__.__name__}: Resampling glucose")
       cgm_history_transformed = cgm_history.groupby('patient_id').apply(pp.cgm_transform, include_groups=False).reset_index(level=0)
       out_file = save_data_as(cgm_history_transformed, 'CSV', os.path.join(out_path_study, f"cgm_history-transformed"))
       tqdm.write(f"[{current_time()}] [x] CGM resampled: {out_file.split('/')[-1]}")
       progress.update(1)
-      tqdm.write("")
 
-tqdm.write("Processing complete.")
 
+if __name__ == "__main__":
+    main()
