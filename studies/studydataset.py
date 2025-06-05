7a4100f089df09def8acc94430d325b4ea7b262a
Jan Wrede <wrede.jan@gmail.com>
2025-06-05 14:33:23 +0200

    [122-doc-update] remove obsolete save functions

--- studies/studydataset.py
+++ studies/studydataset.py
@@ -132,43 +132,6 @@ def validate_cgm_output_dataframe(func):
         return df
     return wrapper
 
-def save_to_csv(df, file_path, compressed):
-    """
-    Save a pandas DataFrame to a csv file. The file can be compressed using gzip.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to save.
-        file_path (str): The path to the output file.
-        compressed (bool): If True, the output file will be compressed using gzip.
-    """
-
-    df.to_csv(file_path + (".csv.gz" if compressed else '.csv'), index=False, 
-                compression='gzip' if compressed else None)
-
-def save_to_parquet_partitioned(df, base_path, study_name, data_type):
-    """
-    Save a pandas DataFrame to Parquet files, partitioned by specified columns.
-
-    The output structure will be using Hive style partitioning, for example:
-    `<base_path>/study_name=Flair/data_type=basal/patient_id=1/9ce850a3b57e49d5a01ca1153db0fb40-0.parquet`
-    Args:
-        df (pd.DataFrame): The DataFrame to save.
-        base_path (str): The base directory for the output files.
-        study_name (str): The name of the study.
-        data_type (str): The type of data being saved.
-    """
-
-    # Save the DataFrame as partitioned Parquet files
-    df = df.assign(study_name=study_name, data_type=data_type)
-    df.to_parquet(
-        base_path,
-        index=False,
-        partition_cols=['study_name', 'data_type', 'patient_id'],
-        engine="pyarrow",  # Ensure compatibility with partitioning
-        compression="snappy",
-        existing_data_behavior='delete_matching'
-    )
-
 class StudyDataset:
     """
     The `StudyDataset` class is designed to represent a clinical diabetes dataset with continuous glucose monitoring and insulin delivery data in the form of boluses and basal rates.
